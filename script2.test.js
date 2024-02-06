6669f1175f26486694f00eab42246b879973b322
Ricardo Passinho <passocurto@gmail.com>
2024-02-06 16:54:43 -0300

    clear: remove unecessary comments

--- script2.test.js
+++ script2.test.js
@@ -1,12 +1,8 @@
 const swapi = require('./script2');
-// const fetch = require('node-fetch'); you no longer need this package if you have the latest version of Node!
+
 
 it('calls swapi to get people', () => {
-  //expect.assertions(number) verifies that a
-  // certain number of assertions are called during a
-  // test. This is often useful when testing asynchronous
-  //code, in order to make sure that assertions in a
-  //callback actually got called.
+  
   expect.assertions(1)
   return swapi.getPeople(fetch).then(data => {
     expect(data.count).toEqual(87);
@@ -36,14 +32,4 @@ it('getPeople returns count and results', () => {
     expect(data.count).toEqual(87);
     expect(data.results.length).toBeGreaterThan(5);
   })
-  // done() 
 })
-
-// Mock functions are also known as "spies", because
-// they let you spy on the behavior of a function that is
-// called indirectly by some other code, rather than just
-// testing the output. You can create a mock function with
-// jest.fn(). If no implementation is given, the mock
-// function will return undefined when invoked.
-
-
